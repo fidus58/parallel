dec309a9b9f2042248c4524ce2d38e5a89469772
fidus58 <ahrens@informatik.hu-berlin.de>
2021-06-29 12:46:11 +0200

    Initial Commit

---
+++ parallel/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  parallel
+//
+//  Created by Klaus Ahrens on 29.06.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
